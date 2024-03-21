e0484194a17010f2bffdb747868d18662b754a3c
CamiloPaguay <164214564+CamiloPaguay@users.noreply.github.com>
2024-03-21 19:35:04 +0100

    Update gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -1,28 +1,20 @@
-import _translate from "./_translate.js"
-const tradutor = _translate.plugins.gc_admins
+let handler = async (m, { conn, participants, groupMetadata, args }) => {
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
+const groupAdmins = participants.filter(p => p.admin)
+const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n▢ ')
+const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 
+let text = `
+≡ *Admis del grupo* _${groupMetadata.subject}_
 
-const handler = async (m, {conn, participants, groupMetadata, args}) => {
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/admins.jpg';
-  const groupAdmins = participants.filter((p) => p.admin);
-  const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
-  const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
-  const pesan = args.join` `;
-  const oi = `${tradutor.texto1[3]} ${pesan}`;
-  const text = `${tradutor.texto1[0]}
-
-${oi}
-
-${tradutor.texto1[1]}
-${listAdmin}
-
-${tradutor.texto1[2]}`.trim();
-  conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
-};
-handler.help = ['admins <texto>'];
-handler.tags = ['group'];
-// regex detect A word without case sensitive
-handler.customPrefix = /a|@/i;
-handler.command = /^(admins|@admins|dmins)$/i;
-handler.group = true;
-export default handler;
+┌─⊷ *ADMINS*
+▢ ${listAdmin}
+└───────────
+`.trim()
+conn.sendFile(m.chat, pp, 'staff.png', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
+}
+handler.help = ['staff']
+handler.tags = ['group']
+handler.command = ['staff', 'admins', 'listadmin'] 
+handler.group = true
+export default handler
