060749fd0b26d291d95f3b9633d584eefed465f1
CamiloPaguay <164214564+CamiloPaguay@users.noreply.github.com>
2024-03-21 19:28:13 +0100

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,38 +1,28 @@
-import _translate from "./_translate.js"
-const tradutor = _translate.plugins.info_groupsofc
-
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `${tradutor.texto1[0]}
+  const text = `*𝗛𝗢𝗟𝗔 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 👋🏻, 𝗧𝗘 𝗜𝗡𝗩𝗜𝗧𝗢 𝗔 𝗨𝗡𝗜𝗥𝗧𝗘 𝗔𝗟 𝗚𝗥𝗨𝗣𝗢 𝗢𝗙𝗜𝗖𝗜𝗔𝗟 𝗗𝗘 𝗠𝗔𝗬-𝗕𝗢𝗧*
 
-${tradutor.texto1[1]}
-${tradutor.texto1[2]}
+*➤ 🤖 𝗚𝗿𝘂𝗽𝗼 𝗼𝗳𝗶𝗰𝗶𝗮𝗹 𝗱𝗲𝗹 𝗕𝗼𝘁:*
 
-${tradutor.texto1[3]}
-
-${tradutor.texto1[4]}
-
-${tradutor.texto1[5]}
-
-${tradutor.texto1[6]}`.trim();
+*1.-* https://chat.whatsapp.com/KXkX5ZzZp3mFWYt5comZHr`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`},
+    'document': {url: `https://github.com/Akanksh`},
     'mimetype': `application/${document}`,
-    'fileName': `${tradutor.texto2}`,
+    'fileName': `「  𝙎𝙊𝙇𝙊 𝙈𝘼𝙔-𝘽𝙊𝙏 」`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+        'mediaUrl': 'https://github.com/Akanksh',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': `${tradutor.texto3}`,
+        'title': '𝗘𝗹 𝗺𝗲𝗷𝗼𝗿 𝗯𝗼𝘁 𝗱𝗲 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'}},
+        'sourceUrl': 'https://www.youtube.com/watch?v=-yc54n9Tv5A'}},
     'caption': text,
     'footer': wm,
     'headerType': 6};
