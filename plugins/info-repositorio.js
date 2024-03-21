051d6b9545c9779b1b297e92ebb9caa90773643a
CamiloPaguay <164214564+CamiloPaguay@users.noreply.github.com>
2024-03-21 19:29:36 +0100

    Update info-repositorio.js

--- plugins/info-repositorio.js
+++ plugins/info-repositorio.js
@@ -1,20 +1,17 @@
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
-import _translate from "./_translate.js"
-const tradutor = _translate.plugins.info_repositorio
-
 const handler = async (m, { conn, args, usedPrefix }) => {
-   const res = await fetch('https://api.github.com/repos/BrunoSobrino/TheMystic-Bot-MD');
+   const res = await fetch('https://api.github.com/repos/Ankstha');
    const json = await res.json();
-   let txt = `${tradutor.texto1[0]}\n\n`;
-      txt += `${tradutor.texto1[1]} ${json?.name || tradutor.texto1[2]}\n\n`;
-      txt += `${tradutor.texto1[3]} ${json?.watchers_count || '-'}\n\n`;
-      txt += `${tradutor.texto1[4]} ${(json?.size / 1024).toFixed(2) || '-'} MB\n\n`;
-      txt += `${tradutor.texto1[5]} ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n\n`;
-      txt += `${tradutor.texto1[6]} ${json?.html_url || tradutor.texto1[7]}\n\n`;
-      txt += `${json?.forks_count || '-'} ${tradutor.texto1[8]} ${json?.stargazers_count || '-'} ${tradutor.texto1[9]} ${json?.open_issues_count || '-'} ${tradutor.texto1[10]}`;
-      txt += `${tradutor.texto1[11]}\n_${usedPrefix}gitclone ${json?.html_url || 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}_`;
-   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});
+   let txt = `_*< INFO - REPOSITORIO />*_\n\n`;
+      txt += `🎉 • *Nombre:* ${json?.name || 'May-Bot-MD'}\n\n`;
+      txt += `🖼️ • *Visitantes:* ${json?.watchers_count || '-'}\n\n`;
+      txt += `🎈 • *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n\n`;
+      txt += `🕰️ • *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n\n`;
+      txt += `💌 • *URL:* ${json?.html_url || 'https://github.com/Akanksh'}\n\n`;
+      txt += `${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues\n\n`;
+      txt += `*[ ℹ️ ] Puedes descargar este repositorio enviando el comando:*\n_${usedPrefix}gitclone ${json?.html_url || 'https://github.com/Akanksh/May-Bot-MD'}_`;
+   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.atom.bio/may-team`, "sourceUrl": `https://www.atom.bio/may-team`}}}, {quoted: m});
 };
 handler.command = ['script', 'repositorio', 'repo']
 export default handler;
