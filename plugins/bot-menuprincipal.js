3f0c06ebc0197a25c51c838721f8717b41fee15d
CamiloPaguay <164214564+CamiloPaguay@users.noreply.github.com>
2024-04-01 19:49:29 +0200

    Update bot-menuprincipal.js

--- plugins/bot-menuprincipal.js
+++ plugins/bot-menuprincipal.js
@@ -1,23 +1,23 @@
-const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
-    try {
-      const users = participants.map((u) => conn.decodeJid(u.id));
-      const quoted = m.quoted ? m.quoted : m;
-      const mime = (quoted.msg || quoted).mimetype || '';
-      const isMedia = /image|video|sticker|audio/.test(mime);
-      const more = String.fromCharCode(8206);
-      const masss = more.repeat(850);
-      const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-      const level = global.db.data.users[m.sender].level || 1;
-      const exp = global.db.data.users[m.sender].exp || 0;
-      const role = global.db.data.users[m.sender].role || '';
-      const limit = global.db.data.users[m.sender].limit || 0;
-      const money = global.db.data.users[m.sender].money || 0;
-      const joincount = global.db.data.users[m.sender].joincount || 0;
-      const user = global.db.data.users[m.sender];
-      const readMore = more.repeat(850);
+  try {
+    const users = participants.map((u) => conn.decodeJid(u.id));
+    const quoted = m.quoted ? m.quoted : m;
+    const mime = (quoted.msg || quoted).mimetype || '';
+    const isMedia = /image|video|sticker|audio/.test(mime);
+    const more = String.fromCharCode(8206);
+    const masss = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const level = global.db.data.users[m.sender].level || 1;
+    const exp = global.db.data.users[m.sender].exp || 0;
+    const role = global.db.data.users[m.sender].role || '';
+    const limit = global.db.data.users[m.sender].limit || 0;
+    const money = global.db.data.users[m.sender].money || 0;
+    const joincount = global.db.data.users[m.sender].joincount || 0;
+    const user = global.db.data.users[m.sender];
+    const readMore = more.repeat(850);
 
-  const htextos = `🔮 𝙈𝘼𝙔-𝘽𝙊𝙏 🔮
+    const htextos = `🔮 𝙈𝘼𝙔-𝘽𝙊𝙏 🔮
 
  📌 *Hola,* ${taguser}
 
@@ -42,20 +42,23 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
   📖 _${usedPrefix}labiblia_ (🔞)
 `;
 
-  if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
-    var mediax = await quoted.download?.();
-    conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users }, { quoted: m });
-  } else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
-    var mediax = await quoted.download?.();
-    conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: m });
-  } else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
-    var mediax = await quoted.download?.();
-    conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mpeg', fileName: `Hidetag.mp3` }, { quoted: m });
-  } else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
-    var mediax = await quoted.download?.();
-    conn.sendMessage(m.chat, { sticker: mediax, mentions: users }, { quoted: m });
-  } else {
-    await conn.relayMessage(m.chat, { extendedTextMessage: { text: `${masss}\n${htextos}\n`, ...{ contextInfo: { externalAdReply: { title: '*[ ⚠ ] Advertencia*', body: '𝙈𝙖𝙮-𝘽𝙤𝙩', thumbnail: imagen1, sourceUrl: 'https://github.com/Anksawhs' } } } } }, { quoted: m });
+    if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
+      var mediax = await quoted.download?.();
+      conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users }, { quoted: m });
+    } else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
+      var mediax = await quoted.download?.();
+      conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: m });
+    } else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
+      var mediax = await quoted.download?.();
+      conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mpeg', fileName: `Hidetag.mp3` }, { quoted: m });
+    } else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
+      var mediax = await quoted.download?.();
+      conn.sendMessage(m.chat, { sticker: mediax, mentions: users }, { quoted: m });
+    } else {
+      await conn.relayMessage(m.chat, { extendedTextMessage: { text: `${masss}\n${htextos}\n`, ...{ contextInfo: { externalAdReply: { title: '*[ ⚠ ] Advertencia*', body: '𝙈𝙖𝙮-𝘽𝙤𝙩', thumbnail: imagen1, sourceUrl: 'https://github.com/Anksawhs' } } } } }, { quoted: m });
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
 
