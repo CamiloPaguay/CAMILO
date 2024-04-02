9668416ffeaf62782cd4df06f046b4847a660375
CamiloPaguay <164214564+CamiloPaguay@users.noreply.github.com>
2024-04-02 19:28:41 +0200

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -4,7 +4,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
-  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+  if (m.text.includes('CAMILOPIEDRA') || m.text.includes('CAMILOPAPEL') || m.text.includes('CAMILOTIJERA') || m.text.includes('CAMILOserbot') || m.text.includes('CAMILOjadibot')) return !0;
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
