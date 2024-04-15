d40c8d54779c002151f5dab0e84bcb32a140a6f5
CamiloPaguay <164214564+CamiloPaguay@users.noreply.github.com>
2024-04-15 17:14:40 +0200

    Update descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -3,10 +3,10 @@ import { tiktokdl } from '@bochilteam/scraper'
 
 var handler = async (m, { conn, text, args, usedPrefix, command}) => {
 
-if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese un enlace de tiktok*\n\nEjemplo, !${command} https://vm.tiktok.com/ZMYG92bUh/`, m)
-if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `🚩 *Verifica que el enlace sea correcto*`, m)
+if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese un enlace de tiktok*\n\nEjemplo, !${command} https://vm.tiktok.com/ZMYG92bUh/`, m, fake, )
+if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `🚩 *Verifica que el enlace sea correcto*`, m, fake, )
 
-    await conn.sendMessage(m.chat, { react: { text: '⏰️', key: m.key } })
+m.react(rwait)
 
 const { key } = await conn.sendMessage(m.chat, {text: `${wait}`}, {quoted: m})
 await delay(1000 * 1)
@@ -23,30 +23,30 @@ let te = `*Nombre:* ${p.nickname}
 *Duración:* ${p.duration}
 *Descripción:* ${p.description}`
 conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
-    await conn.sendMessage(m.chat, { react: { text: '✅️', key: m.key } })
+m.react(done)
 } catch {
 
 try {
 
 const { author: { nickname }, video, description } = await tiktokdl(args[0])
 const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-    await conn.sendMessage(m.chat, { react: { text: '❌️', key: m.key } })
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-if (!url) return conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m)
-conn.sendFile(m.chat, url, 'fb.mp4', `*Nombre:* ${nickname}\n*Descripción:* ${description}`, m)
-    await conn.sendMessage(m.chat, { react: { text: '✅️', key: m.key } })
-} catch {
-    await conn.sendMessage(m.chat, { react: { text: '❌️', key: m.key } })
-conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m)
-}}
 
+m.react(error)
+if (!url) return conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
+conn.sendFile(m.chat, url, 'fb.mp4', `*Nombre:* ${nickname}\n*Descripción:* ${description}`, m)
+m.react(done)
+} catch {
+m.react(error)
+conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
+}}
+    
 }
 handler.help = ['tiktok']
 handler.tags = ['descargas']
 handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
 
-//handler.limit = true
-//handler.register = true
+handler.limit = true
+handler.register = true
 
 export default handler
 
