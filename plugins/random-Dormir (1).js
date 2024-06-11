3c10f1a15d5420f609e0174b69aa8ce992cc6d71
CamiloPaguay <164214564+CamiloPaguay@users.noreply.github.com>
2024-06-11 17:17:33 -0500

    Update random-Dormir (1).js

--- plugins/random-Dormir (1).js
+++ plugins/random-Dormir (1).js
@@ -1,36 +1,23 @@
-const handler = async (m, { conn, text, command }) => {
-  try {
-    const yh = global.lolix;
-
-    if (!Array.isArray(yh) || yh.length === 0) {
-      throw new Error("La lista de imágenes no está disponible.");
-    }
-
-    const url = yh[Math.floor(Math.random() * yh.length)];
-    const mensajes = [
-      '_*Descansa, mañana será un mejor día*_',
-      '_*Que tengas un buen descanso*_',
-      '_*Espero que tengas dulces sueños*_',
-      '_*Descansa y recarga energías para mañana*_',
-      '_*Que tengas un reparador descanso*_',
-      '_*Buenas noches, que descanses*_',
-    ];
-    const mensaje = mensajes[Math.floor(Math.random() * mensajes.length)];
-    await conn.sendMessage(m.chat, { image: { url: url }, caption: mensaje }, { quoted: m });
-  } catch (error) {
-    console.error("Error al enviar el mensaje:", error);
-    await conn.sendMessage(m.chat, "Lo siento, ocurrió un error al intentar enviarte una imagen. Por favor, inténtalo de nuevo más tarde.", { quoted: m });
-  }
+const handler = async (m, {conn, text, command}) => {
+  const yh = global.loliz;
+  const url = yh[Math.floor(Math.random() * yh.length)];
+  const mensajes = [
+    '_*Descansa, mañana será un mejor día*_',
+    '_*Que tengas un buen descanso*_',
+    '_*Espero que tengas dulces sueños*_',
+    '_*Descansa y recarga energías para mañana*_',
+    '_*Que tengas un reparador descanso*_',
+    '_*Buenas noches, que descanses*_',
+  ];
+  const mensaje = mensajes[Math.floor(Math.random() * mensajes.length)];
+  conn.sendMessage(m.chat, {image: {url: url}, caption: mensaje}, {quoted: m});
 };
-
 handler.command = /^(dormir|Dormir|Mimir|mimir|sueño)$/i;
 handler.tags = ['dormir'];
 handler.help = ['dormir'];
-
 export default handler;
 
-// Lista de imágenes global
-global.lolix = [
+global.loliz = [
   'https://i.ibb.co/PZvZgnd/Whats-App-Image-2024-02-18-at-5-11-51-PM-1.jpg',
   'https://i.ibb.co/gJx2Fvx/Whats-App-Image-2024-02-18-at-5-11-51-PM-2.jpg',
   'https://i.ibb.co/Yk9QjtR/Whats-App-Image-2024-02-18-at-5-11-51-PM-3.jpg',
